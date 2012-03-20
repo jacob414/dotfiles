cfde6aa8167cd36b4f86c8c47968f84f99e8c6a9
Jacob Oscarson <jacob@plexical.com>
2012-03-20 10:53:39 +0100

    Added parsing as separate functions

--- bin/hours.py
+++ bin/hours.py
@@ -11,6 +11,37 @@ def dot_times(dotted):
         print(e)
         sys.exit(1)
 
+def units(tim):
+    if tim.endswith('min'):
+        return int(tim[0:1])
+    elif tim.endswith('h'):
+        return 60 * int(tim[0:1])
+    else:
+        return 0
+
+def minutes(timing):
+    mins = 0
+    for tim in timing.strip().split(' '):
+        if tim.endswith('min'):
+            mins += int(tim.replace('min', ''))
+        elif tim.endswith('h'):
+            mins += int(tim.replace('h', '')) * 60
+
+    return mins
+
+def parse_line(line):
+    desc, timing = line.split(':')
+    desc = desc.split(',')
+    if len(desc) == 1:
+        project = desc[0]
+        label = 'unknown'
+    elif len(desc) == 2:
+        project, label = desc
+    else:
+        project, label = 'unknown'
+        pass
+    return project, label.strip(), minutes(timing)
+
 for line in (l.strip() for l in times.readlines()):
     if line.startswith('20'):
         isoday = line

2e49bdf1a0be6e999127e241b1e3d6b8986c0e42
Jacob Oscarson <jacob@plexical.com>
2015-09-25 12:40:00 +0200

    Started on an empty host shell init file for `goldskip`

---
+++ shell/goldskip.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+source ~/src/mine/dotfiles/shell/ext/git-completion.bash
+
+# From
+# http://www.leancrew.com/all-this/2013/02/getting-rid-of-open-with-duplicates/
+alias fixopenwith='"/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister" -kill -r -domain local -domain system -domain user'
+
+function listening {
+    lsof -n -i4TCP:$1 | grep LISTEN
+}
+
+function qv {
+    qlmanage -p $1 2> /dev/null # spams stderr
+}

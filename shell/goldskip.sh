aa1a510dd34040c2e8cbef9c2bfa529577e44568
Jacob Oscarson <jacob@plexical.com>
2016-05-13 14:36:59 +0200

    Another update b/c new(ish) computer

--- shell/goldskip.sh
+++ shell/goldskip.sh
@@ -1,18 +1,51 @@
 #!/bin/sh
+if [ -n `which gls` ]; then
+    export PATH=/usr/local/bin:$PATH
+fi
+
+BREW=/usr/local
+CELLAR=$BREW/Cellar
+
+export PATH=$CELLAR/python/2.7.10_2/bin:$PATH
+export PATH=/usr/local/share/npm/bin:$PATH
+export PATH=~/.cabal/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+
+export EDITOR=emacsclient
+
+export NODE_PATH=/usr/local/share/npm/lib/node_modules
+
+. /usr/local/Cellar/coreutils/8.7/aliases
+unalias ls
+alias ls='gls --color=auto'
+
+alias smallrd='diskutil erasevolume HFS+ "ramdisk" `hdiutil attach -nomount ram://200000`'
+
+ # Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+rvm use 2.0.0
+
 source ~/src/mine/dotfiles/shell/ext/git-completion.bash
 
 # From
 # http://www.leancrew.com/all-this/2013/02/getting-rid-of-open-with-duplicates/
 alias fixopenwith='"/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister" -kill -r -domain local -domain system -domain user'
 
+# https://github.com/basecamp/pow/issues/412
+alias powfix='touch ~/.pow/restart.txt'
+
 function listening {
     lsof -n -i4TCP:$1 | grep LISTEN
 }
+
+# OPAM configuration
+source ~/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
 
 function qv {
     qlmanage -p $1 2> /dev/null # spams stderr
 }
 
-export EDITOR="/usr/local/Cellar/emacs/24.5/bin/emacsclient"
-
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# NVM configuration
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
--- shell/profile.sh
+++ shell/profile.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 . ~/src/mine/dotfiles/shell/baseenv.sh
 . ~/src/mine/dotfiles/shell/functions.sh
-. ~/src/mine/dotfiles/shell/superfly.sh
+. ~/src/mine/dotfiles/shell/goldskip.sh

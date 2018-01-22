df8dbf20639d206e38fe502eed482855710dd26d
Jacob Oscarson <jacob.oscarson@glooko.com>
2018-01-22 19:28:48 +0100

    Started a shell forever-log (see https://spin.atomicobject.com/2016/05/28/log-bash-history/)

--- shell/baseenv.sh
+++ shell/baseenv.sh
@@ -7,6 +7,7 @@ if [ -z "$CONF" ]; then
 fi
 
 DOTFILES=~/src/mine/dotfiles/shell
+MINE=~/src/mine/skunkworks
 
 export PS1='\[\033[01;32m\]\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
@@ -40,5 +41,7 @@ alias oe-mode='source $OEBASE/scripts/oerc.sh'
 alias oe-mongo='mongod run --config ~/lnk/oe/conf/mongod.conf'
 
 bind '"\C-l":"ls\n"'
+
+export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo -e "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history 1)" >> $MINE/arkiv/shell.txt; fi'
 
 export CDPATH=.:~/src:~:~/src/mine
